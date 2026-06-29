2ba3d7a6bd2dfa391c10a9562b1568f4bfafdf1e
Prakasha Bhajantri <prakash.bhajantri87@gmail.com>
2026-06-29 12:54:41 +0530

    initial commit

---
+++ test.py
@@ -0,0 +1,91 @@
+# Variables
+# int - 1,2,3,4,5,
+# flot - 1.2,1.4
+# string - "hi"
+# char - 'h'
+# bool - False or True
+
+# print("Hello world 4")
+
+# a = 5 # int
+# b = 4.5 # float
+
+# c = a + b
+
+# c = "a"+"bwsfwedf"
+
+# print(c)
+
+# Operators
+
+# + * - /
+# = < > != >= <=
+
+# country = "Japan"
+# condition statements
+
+# if country == 'japan':
+#     # then
+#     continent = 'Asia'
+# elif country == 'USA':
+#     continent = 'NAM'
+# else:
+#     continent = 'other'
+
+
+# a = 5
+# b = 4.5
+
+# # operation = '+'
+# operation = 'AAAA'
+
+# if operation == '+': # False
+#     print("You have choosen " + operation)
+#     print(a + b)
+# elif operation == '-': # True
+#     print("You have choosen " + operation)
+#     print(a - b)
+# elif operation == '*':
+#     print("You have choosen " + operation)
+#     print(a * b)
+# elif operation == '/':
+#     print("You have choosen " + operation)
+#     print(a / b)    
+# else:
+#     print("You have choosen " + operation)
+#     print("Invalid operator")
+
+# Loops and range
+
+# a = 5
+# b = 4.5
+
+# for i in range(0,10): # start = 0, end = 10 (excluding)
+#     print("Iteration--> ", i)
+#     print('HI') # operation
+
+# while
+# a = 5
+
+# while a != 0:
+#     print("Iteration--> ", a)
+#     a = a - 1
+
+# Data Structures - Lists, Tuples, Sets, Dictionary
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
