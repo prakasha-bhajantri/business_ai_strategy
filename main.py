bb598aa697f3a94e0a163545c4f0d07ceb0e175a
Prakasha Bhajantri <prakash.bhajantri87@gmail.com>
2026-07-06 11:08:38 +0530

    added code

--- main.py
+++ main.py
@@ -1,52 +1,14 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from utilities import parse_first_two_letters_from_name
 
-user = FastAPI()
+app = FastAPI()
 
-class Student(BaseModel):
-    name:str
-    age:int
-    course: str
+@app.post("/student")
+def create_student(name):
+    
+    parsed_name = parse_first_two_letters_from_name(name)
 
-@user.get("/")
-def home():
     return {
-        "message": "Welcome to AI Dev"
-    }
-
-@user.get("/student")
-def get_student_():
-    return {
-        "name": "A",
-        "course": "Business Mgt",
-        "completed": False
-    }
-
-@user.get("/student/{student_id}")
-def get_student(student_id):
-    return {
-        "student_id": student_id,
-        "name": "B",
-        "course": "AI"
-    }
-
-
-@user.get("/search")
-def search(name):
-    return {
-        "search": name
-    }
-
-@user.get("/course")
-def course(topic, duration):
-    return {
-        "topic": topic,
-        "duration": duration
-    }
-
-@user.post("/student")
-def create_student(student:Student):
-    return {
-        "message": "Student is created",
-        "student": student
+        "Name": name,
+        "Parsed Name": parsed_name
     }
---
+++ utilities.py
@@ -0,0 +1,13 @@
+def parse_first_two_letters_from_name(name):
+    # 
+    print(f"Before Parsing the name: {name}")
+
+    parse_name = name[0:2]
+
+    print(f"After Parsing the name: {name}")
+
+    return parse_name
+
+
+
+
